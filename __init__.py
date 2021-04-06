9af528f7f291f3c48847773ca191440ab1b740da
zhang0927 <zxh_0927@163.com>
2021-04-06 19:27:58 +0800

    修改

--- __init__.py
+++ __init__.py
@@ -1,13 +1,12 @@
-import requests
-import unittest
-import MySQLdb
 import os,sys
-
 dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(dir)
 sys.path.append("F:\\python\\YitihuaInterface\\venv\\Lib\\site-packages")
 
 
+import requests
+import unittest
+import MySQLdb
 class Test(unittest.TestCase):
 
 
